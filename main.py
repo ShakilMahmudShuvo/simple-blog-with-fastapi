5d69a89accd5a773aa27f79b79780ce98c8d8a99
ShakilMahmudShuvo <shakil.mahmud@ideascale.com>
2023-11-27 10:46:49 +0600

    initial commit

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-    return {'data': {'name': 'shakil'}}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
 
-@app.get('/about')
-def about():
-    return {'data': {'About US'}}
+@app.get('/blog/unpublished')
+def unpublished():
+    return {'data': 'All the unpublished blogs!'}
+
+
+@app.get('/blog/{id}')
+def show(id: int):
+    return {'data': id}
+
+
+@app.get('/blog/{id}/comments')
+def comments():
+    return {'data': {'1', '2'}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blogs(blog: Blog): # blog is the request instance
+    return {'data': f"Blog is created with title {blog.title}"}
+
