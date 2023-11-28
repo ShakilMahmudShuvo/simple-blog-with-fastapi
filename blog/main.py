4accc279b9986084b0fffe2bbacfcd9726acfc26
ShakilMahmudShuvo <shakil.mahmud@ideascale.com>
2023-11-28 15:06:04 +0600

    add put and delete operations for blogs

--- blog/main.py
+++ blog/main.py
@@ -24,17 +24,36 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     return new_blog
 
 
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with this id {id} is missing!')
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return 'done'
+
+
+@app.put('/blog/{id}', status_code= status.HTTP_202_ACCEPTED)
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id ==id)
+    if not blog.first():
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f'Blog with this id {id} is missing!')
+    blog.update(request.dict())
+    db.commit()
+    return 'Update done'
+
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
 
-@app.get('/blog/{id}', status_code= status.HTTP_200_OK)
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
 def show(id: int, response: Response, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"Blog with this id {id} not found!")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with this id {id} not found!")
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {'detial': f"Blog with this id {id} not found!"}
     return blog
